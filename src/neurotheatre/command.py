decd61bb889e634e65c67bf370ae20f078e7216f
Griffin Milsap <griffin.milsap@jhuapl.edu>
2025-05-17 11:35:06 -0400

    reordered args

--- src/neurotheatre/command.py
+++ src/neurotheatre/command.py
@@ -10,15 +10,15 @@ from neurotheatre.osc import OSCSystem, OSCSystemSettings, EEGOSCSettings
 def osc():
 
     parser = argparse.ArgumentParser(description = 'unicorn OSC client')
+    parser.add_argument('-d', '--device', help = 'device address', default = 'simulator')
     parser.add_argument('-a', '--address', help = 'remote OSC server address', default = 'localhost')
     parser.add_argument('-p', '--port', help = 'remote OSC server port (UDP)', default = 8000, type = int)
-    parser.add_argument('-d', '--device', help = 'device address', default = 'simulator')
     parser.add_argument('--blocksize', help = 'eeg sample block size @ 200 Hz', default = 10, type = int)
 
     class Args:
+        device: str
         address: str
         port: int
-        device: str
         blocksize: int
 
     args = parser.parse_args(namespace = Args)
