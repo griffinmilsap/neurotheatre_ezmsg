8f940cb7e8a38637feae944c6bfc8594df67671e
Griffin Milsap <griffin.milsap@jhuapl.edu>
2025-05-17 11:34:14 -0400

    initial OSC impl

--- src/neurotheatre/command.py
+++ src/neurotheatre/command.py
@@ -5,23 +5,33 @@ import ezmsg.core as ez
 from ezmsg.unicorn.device import UnicornSettings
 from ezmsg.panel.application import Application, ApplicationSettings
 
-from neurotheatre.osc import OSCSystem, OSCSystemSettings
+from neurotheatre.osc import OSCSystem, OSCSystemSettings, EEGOSCSettings
 
 def osc():
 
     parser = argparse.ArgumentParser(description = 'unicorn OSC client')
-
-    parser.add_argument('-a', '--address', help = 'device address', default = 'simulator')
+    parser.add_argument('-a', '--address', help = 'remote OSC server address', default = 'localhost')
+    parser.add_argument('-p', '--port', help = 'remote OSC server port (UDP)', default = 8000, type = int)
+    parser.add_argument('-d', '--device', help = 'device address', default = 'simulator')
+    parser.add_argument('--blocksize', help = 'eeg sample block size @ 200 Hz', default = 10, type = int)
 
     class Args:
         address: str
+        port: int
+        device: str
+        blocksize: int
 
     args = parser.parse_args(namespace = Args)
 
     osc = OSCSystem(
         OSCSystemSettings(
-            UnicornSettings(
-                address = args.address
+            osc_settings = EEGOSCSettings(
+                address = args.address,
+                port = args.port
+            ),
+            unicorn_settings = UnicornSettings(
+                address = args.device,
+                n_samp = args.blocksize
             )
         )
     )
--- src/neurotheatre/osc.py
+++ src/neurotheatre/osc.py
@@ -6,25 +6,43 @@ from ezmsg.unicorn.device import UnicornSettings
 from ezmsg.util.messages.axisarray import AxisArray
 from ezmsg.util.debuglog import DebugLog
 
+from pythonosc.udp_client import SimpleUDPClient
 
-class OSCClientSettings(ez.Settings):
-    ...
 
-class OSCClient(ez.Unit):
-    SETTINGS = OSCClientSettings
+class EEGOSCSettings(ez.Settings):
+    port: int
+    address: str = 'localhost'
+    time_axis: str = 'time'
+
+class EEGOSCState(ez.State):
+    client: SimpleUDPClient
+
+class EEGOSC(ez.Unit):
+    SETTINGS = EEGOSCSettings
+    STATE = EEGOSCState
 
     INPUT_SIGNAL = ez.InputStream(AxisArray)
+    INPUT_MOTION = ez.InputStream(AxisArray)
 
     async def initialize(self) -> None:
-        ...
+        self.STATE.client = SimpleUDPClient(
+            address = self.SETTINGS.address, 
+            port = self.SETTINGS.port
+        )
 
     @ez.subscriber(INPUT_SIGNAL)
     async def on_signal(self, msg: AxisArray):
-        ez.logger.info('Got Data!')
+        ...
 
-    
+    @ez.subscriber(INPUT_MOTION)
+    async def on_motion(self, msg: AxisArray):
+        for aa in msg.iter_over_axis(self.SETTINGS.time_axis):
+            self.STATE.client.send_message('/imu/accel', aa.data[0:3].tolist())
+            self.STATE.client.send_message('/imu/gyro', aa.data[3:6].tolist())
+
 
 class OSCSystemSettings(ez.Settings):
+    osc_settings: EEGOSCSettings
     unicorn_settings: UnicornSettings
 
 class OSCSystem(ez.Collection):
@@ -32,6 +50,7 @@ class OSCSystem(ez.Collection):
     SETTINGS = OSCSystemSettings
 
     DASHBOARD = UnicornDashboard()
+    OSC = EEGOSC()
     LOG = DebugLog()
 
     def configure(self) -> None:
@@ -40,8 +59,10 @@ class OSCSystem(ez.Collection):
                 device_settings = self.SETTINGS.unicorn_settings
             )
         )
+        self.OSC.apply_settings(self.SETTINGS.osc_settings)
 
     def network(self) -> ez.NetworkDefinition:
         return (
-            (self.DASHBOARD.OUTPUT_SIGNAL, self.LOG.INPUT),
+            (self.DASHBOARD.OUTPUT_SIGNAL, self.OSC.INPUT_SIGNAL),
+            (self.DASHBOARD.OUTPUT_MOTION, self.OSC.INPUT_MOTION),
         )
